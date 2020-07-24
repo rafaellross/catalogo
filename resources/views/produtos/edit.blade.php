3cde0a298fce2f408f365fc64b4c644ba2b11fd1
Rafael Ross <rafaellross@Rafaels-Air.hub>
2020-07-24 23:03:08 +1000

    Adicionado preco ao produto

--- resources/views/produtos/edit.blade.php
+++ resources/views/produtos/edit.blade.php
@@ -70,7 +70,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Update') }}
+                                    {{ __('Atualizar') }}
                                 </button>
                             </div>
                         </div>
